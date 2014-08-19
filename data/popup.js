4480a4e1a92a9e26ac1600ff022000cf856de929
sandodo <zouxiong@gmail.com>
2014-08-19 10:56:13 +0800

    post and recv msg between main.js and popup.js successfully

--- data/popup.js
+++ data/popup.js
@@ -1,22 +1,22 @@
 // popup.js
 
-var SingapuRateOptionsHandler = {
-
-};
-
 // Run our script as soon as the panel is ready.
 self.port.on("show", function onShow() {
-
+	return;
 });
 
 // Run our script as soon as the panel is ready.
 self.port.on("initMsg", function(msg) {
-	var resArray = msg.split("|||");
+
+	var sRcvMsg = msg;
+	var resArray = sRcvMsg.split("|||");
 	var sUserName = "";
 	var sUserBirthday = "";
 	var sAuthenticate = "";
 	var sCurDomainUrlIdx = "";
+	var sSuperSafeMode = "";
 	var sVoteId = "";
+	
 	for(var i=0; i < resArray.length; i++) 
 	{
 		var strTemp = resArray[i];
@@ -33,6 +33,10 @@ self.port.on("initMsg", function(msg) {
 		{
 			sUserBirthday = sTempKeyValuePair[1];
 		}
+		else if (sTempKeyValuePair[0] == "c_supersafe")
+		{
+			sSuperSafeMode = sTempKeyValuePair[1];
+		}
 		else if (sTempKeyValuePair[0] == "authenticate")
 		{
 			sAuthenticate = sTempKeyValuePair[1];
@@ -40,24 +44,22 @@ self.port.on("initMsg", function(msg) {
 		else if (sTempKeyValuePair[0] == "curDomainUrlIdx")
 		{
 			sCurDomainUrlIdx = sTempKeyValuePair[1];
-			if(sCurDomainUrlIdx == "")
-				sCurDomainUrlIdx = "-1";
 		}
 		else if (sTempKeyValuePair[0] == "voteId")
 		{
 			sVoteId = sTempKeyValuePair[1];
 		}
 	}
+	//*/
 	if(sCurDomainUrlIdx == "")
 		sCurDomainUrlIdx = "-1";
 	if(sVoteId == "")
 		sVoteId = "0";		
-		
+			
 	document.getElementById("c_username").value = sUserName;
 	document.getElementById("c_birthday").value = sUserBirthday;
 	
-  	if( sAuthenticate === true 
-    	|| sAuthenticate == "true"  )
+  	if( sAuthenticate == "true"  )
   	{
     	document.getElementById("c_username").setAttribute("disabled","disabled");
     
@@ -66,16 +68,16 @@ self.port.on("initMsg", function(msg) {
         var sWelcomeMessage = "SingapuRate_welcome_local_acct";
     	document.getElementById("label_username").textContent = sWelcomeMessage;
     
-    	//if(SingapuRateSS.storage[ SingapuRateUtils.SR_Utilities.SingapuRatePrefKeySuperSafeMode ] == "yes" )    													
-    	//{
-      	//	document.getElementById("c_supersafe").setAttribute("checked","checked");
-      	//	document.getElementById("c_supersafe").value = "yes";
-    	//}
-    	//else
-    	//{
-	  	//	document.getElementById('c_username').removeAttribute("checked");	
-      	//	document.getElementById("c_supersafe").value = "no";
-    	//}
+    	if(sSuperSafeMode == "yes" )    													
+    	{
+      		document.getElementById("c_supersafe").setAttribute("checked","checked");
+      		document.getElementById("c_supersafe").value = "yes";
+    	}
+    	else
+    	{
+	  		document.getElementById('c_username').removeAttribute("checked");	
+      		document.getElementById("c_supersafe").value = "no";
+    	}
   	}
   	else
   	{
@@ -84,7 +86,7 @@ self.port.on("initMsg", function(msg) {
     	document.getElementById("c_supersafe").setAttribute("checked","checked");
     	document.getElementById("c_supersafe").value = "yes";
   	}
-	
+ 		
 	var iUrlIdx = parseInt(sCurDomainUrlIdx);
 	var iVoteId = parseInt(sVoteId);
 	if(iUrlIdx >= 0 && iVoteId <= 0 )
@@ -109,8 +111,10 @@ self.port.on("initMsg", function(msg) {
     	document.getElementById("b_vote_opinion").setAttribute("type","hidden");
     	document.getElementById("b_why_matters").setAttribute("style","vertical-align: top; width: 1px; height: 1px;");	
     	document.getElementById("b_why_matters").setAttribute("type","hidden");
-  	}		
+  	}	
+  	
 });
+
 
 window.onload = function() {
   document.getElementById("b_save").onclick = function() {
@@ -118,7 +122,6 @@ window.onload = function() {
 	var password = document.getElementById("c_password").value.trim();
 	var birthday = document.getElementById("c_birthday").value.trim();
 	var supersafe = document.getElementById("c_supersafe").value.trim();
-	var bLogin = false;
 	
 	if( username.length < 4 || password.length < 4 )
 	{
@@ -139,159 +142,20 @@ window.onload = function() {
 		}
 		else
 		{
-			/*
-			if( SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
-					|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true"  )
-			{
-		        var passwordCombineStr = username.trim() + password.trim() + 'nh4da68h4jf6s4kj8g6d4df8b4d5';
-				var passwordEncrypted = SingapuRateUtils.SR_Utilities.hex_sha256(passwordCombineStr);
-				if( passwordEncrypted == SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAthentCode] )
-				{
-					//authenticate the user with password, proceed to update profile
-				}
-				else
-				{
-					//failed to authenticate
-	  	  			//alert(chrome.i18n.getMessage("SingapuRate_update_fail"));
-	  	  			alert("SingapuRate_update_fail");
-					return;
-				}
-			}
-			//*/
+			//compose the string for profile save
+			var sPostMsg = "";
+			sPostMsg += "c_username" + "===" + username + "|||";
+			sPostMsg += "c_password" + "===" + password + "|||";
+			sPostMsg += "c_birthday" + "===" + birthday + "|||";
+			sPostMsg += "c_supersafe" + "===" + supersafe + "|||";
 			
-			bLogin = true;
+			self.port.emit("save", sPostMsg);
+
 		}
 	}
-	
-	/*
-	if(birthday != SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyBirthday]
-			|| supersafe != SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeySuperSafeMode]
-			|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === false 
-			|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "false" )
-	{
-		//store username
-		//store birthday
-		//store password
-		//store super safe mode
-		SingapuRateUtils.SR_Prefs.storePrefs(bLogin, username, birthday, password, supersafe);
-		
-		//alert(chrome.i18n.getMessage("SingapuRate_update_success", [ SingapuRateUtils.SR_Utilities.getUserAge(SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyBirthday]) ]));
-		alert("SingapuRate_update_success");
-	}
-	//*/
-			
-    if(bLogin === true)
-		window.close();
 		
     return;
   };
-  
-  document.getElementById("b_deregister").onclick = function() {
-    var username = document.getElementById("c_username").value.trim();
-	var password = document.getElementById("c_password").value.trim();
-	var birthday = document.getElementById("c_birthday").value.trim();
-	
-	if( username.length < 4 || password.length < 4 )
-	{
-	  //it is a wrong username or password
-	  //alert(chrome.i18n.getMessage("SingapuRate_wrong_username_or_password", [4]));
-	  alert("SingapuRate_wrong_username_or_password");
-	  
-	  return;
-	}
-	else
-	{
-		/*
-		if( SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
-				|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true"  )
-		{
-	        var passwordCombineStr = username.trim() + password.trim() + 'nh4da68h4jf6s4kj8g6d4df8b4d5';
-			var passwordEncrypted = SingapuRateUtils.SR_Utilities.hex_sha256(passwordCombineStr);
-			if( passwordEncrypted == SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAthentCode] )
-			{
-				//authenticate the user with password, proceed to deregister the profile
-			}
-			else
-			{
-				//failed to authenticate
-	  			//alert(chrome.i18n.getMessage("SingapuRate_logout_fail", [username]));
-	  			alert("SingapuRate_logout_fail");
-				
-				return;
-			}
-		}
-		else
-		{//*/
-  			//alert(chrome.i18n.getMessage("SingapuRate_already_logout"));
-  			alert("SingapuRate_already_logout");
-			return;
-		//}
-	}
-	
-	/*
-	if(SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
-			|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true" )
-	{
-		//alert(chrome.i18n.getMessage("SingapuRate_logout_success", [ username ]));
-		alert("SingapuRate_logout_success");
-		//store username
-		//store birthday
-		//store encrypted password
-		SingapuRatePrefs.storePrefs(false, username, birthday, "", "no");
-	}
-	//*/
-			
-	window.close();
-		
-    return;	  
-  };
-
-  document.getElementById("c_supersafe").onclick = function() {
-	var supersafe = document.getElementById("c_supersafe").value.trim();  
-    if( supersafe == "yes" )
-    {
-	  document.getElementById("c_supersafe").value = "no";
-    }
-    else
-    {
-      document.getElementById("c_supersafe").value = "yes"; 
-    }
-  };
-  
-  
-  document.getElementById("b_vote_opinion").onclick = function() {
-	
-	/*
-	var voteUrl = "http://" + SingapuRateUtils.SR_Utilities.SingapuRateDomainName;
-  
-	var strKeyCurDomainUrlIdx 	= SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyCurDomainUrlIdx;
-	
-	var iUrlIdx = SingapuRateSS.storage[strKeyCurDomainUrlIdx];
-
-	if(iUrlIdx >= 0)
-	{
-		var strKeyCLCtgryIdsWI 		= SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyCLCtgryIds + iUrlIdx;
-		var strKeyCLIdxToDomainsWI 	= SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyCLIdxToDomains + iUrlIdx;	
-	
-		var sOnlyDomainName 	= SingapuRateSS.storage[strKeyCLIdxToDomainsWI];
-		var ctgryId 			= SingapuRateSS.storage[strKeyCLCtgryIdsWI];
-	    voteUrl = "http://" + SingapuRateUtils.SR_Utilities.SingapuRateDomainName + "/posting.php?wrs_url=" + sOnlyDomainName + "&wrs_rc=" + ctgryId;
-    	
-	}
-	//chrome.tabs.create( { url : voteUrl  }, function() {} ); 
-   	window.close();
-   	//*/
-	
-  };
-
-  document.getElementById("b_why_matters").onclick = function() {
-
-	//var voteUrl = "http://" + SingapuRateUtils.SR_Utilities.SingapuRateDomainName + "/viewtopic.php?f=12&t=1280";
-	//chrome.tabs.create( { url : voteUrl  }, function() {} ); 
-    window.close();
-  };
       
-}
-
-
+};
 
