7bc14bd7ea7c3d0c1f0d9d1770b6c3321b21cb39
sandodo <zouxiong@gmail.com>
2014-08-19 09:16:09 +0800

    send one short of all init information of panel.

--- data/popup.js
+++ data/popup.js
@@ -6,80 +6,58 @@ var SingapuRateOptionsHandler = {
 
 // Run our script as soon as the panel is ready.
 self.port.on("show", function onShow() {
-	/*
-  //load the preference and display to html
-  document.getElementById("c_username").value = SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAcctName];
-  document.getElementById("c_birthday").value = SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyBirthday];
 
-  //we will need to disable modification of username if already registered
-  if( SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
-    || SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true"  )
-  {
-    document.getElementById("c_username").setAttribute("disabled","disabled");
-    
-    var sWelcomeMessage = chrome.i18n.getMessage("SingapuRate_welcome_local_acct", [ SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAcctName], 
-                                                                                     SingapuRateUtils.SR_Utilities.getUserAge(SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyBirthday]) ]);
-    document.getElementById("label_username").textContent = sWelcomeMessage;
-    
-    if(SingapuRateSS.storage[ SingapuRateUtils.SR_Utilities.SingapuRatePrefKeySuperSafeMode ] == "yes" )    													
-    {
-      document.getElementById("c_supersafe").setAttribute("checked","checked");
-      document.getElementById("c_supersafe").value = "yes";
-    }
-    else
-    {
-	  document.getElementById('c_username').removeAttribute("checked");	
-      document.getElementById("c_supersafe").value = "no";
-    }
-  }
-  else
-  {
-    document.getElementById('c_username').removeAttribute("disabled");	  
-    document.getElementById('b_deregister').setAttribute("disabled","disabled");	  
-    document.getElementById("c_supersafe").setAttribute("checked","checked");
-    document.getElementById("c_supersafe").value = "yes";
-  }
-
-  var strKeyCurDomainUrlIdx 	= SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyCurDomainUrlIdx;
-  var iUrlIdx = SingapuRateSS.storage[strKeyCurDomainUrlIdx];
-  var strKeyCLVoteIdsWI = SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyCLVoteIds + iUrlIdx;
-  var iVoteId = parseInt(SingapuRateSS.storage[strKeyCLVoteIdsWI]);
-  
-  if(iUrlIdx >= 0 && iVoteId <= 0 )
-  {
-    document.getElementById("c_divider_line").setAttribute("style","background-image:url('skin/grey_line.jpg');");	
-    
-    document.getElementById("c_vote_buttons").setAttribute("style","vertical-align: top; width: 50px;");	
-    
-    document.getElementById("b_vote_opinion").setAttribute("style","vertical-align: top; width: 50px;");	
-    document.getElementById("b_vote_opinion").setAttribute("type","image");	
-    document.getElementById("b_vote_opinion").setAttribute("src","skin/vote_opinion.png");	
-
-    document.getElementById("b_why_matters").setAttribute("style","vertical-align: top; width: 50px;");	
-    document.getElementById("b_why_matters").setAttribute("type","image");	
-    document.getElementById("b_why_matters").setAttribute("src","skin/why_matters.png");
-        
-  }
-  else
-  {
-    document.getElementById("c_vote_buttons").setAttribute("style","vertical-align: top; width: 1px;");	
-    document.getElementById("b_vote_opinion").setAttribute("style","vertical-align: top; width: 1px; height: 1px;");	
-    document.getElementById("b_why_matters").setAttribute("style","vertical-align: top; width: 1px; height: 1px;");	
-  }
-	//*/
 });
 
-self.port.on("c_username", function(msg) {
-	document.getElementById("c_username").value = msg;
-});
-
-self.port.on("c_birthday", function(msg) {
-	document.getElementById("c_birthday").value = msg;
-});
-self.port.on("authenticate", function(msg) {
-  	//we will need to disable modification of username if already registered
-  	if( msg === true 
-    	|| msg == "true"  )
+// Run our script as soon as the panel is ready.
+self.port.on("initMsg", function(msg) {
+	var resArray = msg.split("|||");
+	var sUserName = "";
+	var sUserBirthday = "";
+	var sAuthenticate = "";
+	var sCurDomainUrlIdx = "";
+	var sVoteId = "";
+	for(var i=0; i < resArray.length; i++) 
+	{
+		var strTemp = resArray[i];
+		var sTempKeyValuePair = strTemp.split("===");
+		if(sTempKeyValuePair.length != 2)
+		{
+			continue;
+		}
+		if(sTempKeyValuePair[0] == "c_username")
+		{
+			sUserName = sTempKeyValuePair[1];
+		}
+		else if (sTempKeyValuePair[0] == "c_birthday")
+		{
+			sUserBirthday = sTempKeyValuePair[1];
+		}
+		else if (sTempKeyValuePair[0] == "authenticate")
+		{
+			sAuthenticate = sTempKeyValuePair[1];
+		}
+		else if (sTempKeyValuePair[0] == "curDomainUrlIdx")
+		{
+			sCurDomainUrlIdx = sTempKeyValuePair[1];
+			if(sCurDomainUrlIdx == "")
+				sCurDomainUrlIdx = "-1";
+		}
+		else if (sTempKeyValuePair[0] == "voteId")
+		{
+			sVoteId = sTempKeyValuePair[1];
+		}
+	}
+	if(sCurDomainUrlIdx == "")
+		sCurDomainUrlIdx = "-1";
+	if(sVoteId == "")
+		sVoteId = "0";		
+		
+	document.getElementById("c_username").value = sUserName;
+	document.getElementById("c_birthday").value = sUserBirthday;
+	
+  	if( sAuthenticate === true 
+    	|| sAuthenticate == "true"  )
   	{
     	document.getElementById("c_username").setAttribute("disabled","disabled");
     
@@ -106,22 +84,9 @@ self.port.on("authenticate", function(msg) {
     	document.getElementById("c_supersafe").setAttribute("checked","checked");
     	document.getElementById("c_supersafe").value = "yes";
   	}
-});
-self.port.on("curDomainUrlIdx", function(msg) {
-	var sLocalMsg = msg;
-	if(sLocalMsg == "")
-		sLocalMsg = "-1";
-	document.getElementById("curDomainUrlIdx").value = sLocalMsg;
-
-});
-self.port.on("voteId", function(msg) {
-	var sLocalMsg = msg;
-	if(sLocalMsg == "")
-		sLocalMsg = "0";
-	document.getElementById("voteId").value = sLocalMsg;
-	var iUrlIdx = parseInt(document.getElementById("curDomainUrlIdx").value);
-	var iVoteId = parseInt(document.getElementById("voteId").value);
-
+	
+	var iUrlIdx = parseInt(sCurDomainUrlIdx);
+	var iVoteId = parseInt(sVoteId);
 	if(iUrlIdx >= 0 && iVoteId <= 0 )
   	{
     	document.getElementById("c_divider_line").setAttribute("style","background-image:url('./data/grey_line.jpg');");	
@@ -144,9 +109,8 @@ self.port.on("voteId", function(msg) {
     	document.getElementById("b_vote_opinion").setAttribute("type","hidden");
     	document.getElementById("b_why_matters").setAttribute("style","vertical-align: top; width: 1px; height: 1px;");	
     	document.getElementById("b_why_matters").setAttribute("type","hidden");
-  	}
+  	}		
 });
-
 
 window.onload = function() {
   document.getElementById("b_save").onclick = function() {
@@ -175,6 +139,7 @@ window.onload = function() {
 		}
 		else
 		{
+			/*
 			if( SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
 					|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true"  )
 			{
@@ -192,11 +157,13 @@ window.onload = function() {
 					return;
 				}
 			}
+			//*/
 			
 			bLogin = true;
 		}
 	}
 	
+	/*
 	if(birthday != SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyBirthday]
 			|| supersafe != SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeySuperSafeMode]
 			|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === false 
@@ -211,6 +178,7 @@ window.onload = function() {
 		//alert(chrome.i18n.getMessage("SingapuRate_update_success", [ SingapuRateUtils.SR_Utilities.getUserAge(SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyBirthday]) ]));
 		alert("SingapuRate_update_success");
 	}
+	//*/
 			
     if(bLogin === true)
 		window.close();
@@ -233,6 +201,7 @@ window.onload = function() {
 	}
 	else
 	{
+		/*
 		if( SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
 				|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true"  )
 		{
@@ -252,13 +221,14 @@ window.onload = function() {
 			}
 		}
 		else
-		{
+		{//*/
   			//alert(chrome.i18n.getMessage("SingapuRate_already_logout"));
   			alert("SingapuRate_already_logout");
 			return;
-		}
+		//}
 	}
 	
+	/*
 	if(SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] === true 
 			|| SingapuRateSS.storage[SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyAuthenticate] == "true" )
 	{
@@ -269,14 +239,11 @@ window.onload = function() {
 		//store encrypted password
 		SingapuRatePrefs.storePrefs(false, username, birthday, "", "no");
 	}
+	//*/
 			
 	window.close();
 		
     return;	  
-  };
-  
-  document.getElementById("b_close").onclick = function() {
-    window.close();
   };
 
   document.getElementById("c_supersafe").onclick = function() {
@@ -294,6 +261,7 @@ window.onload = function() {
   
   document.getElementById("b_vote_opinion").onclick = function() {
 	
+	/*
 	var voteUrl = "http://" + SingapuRateUtils.SR_Utilities.SingapuRateDomainName;
   
 	var strKeyCurDomainUrlIdx 	= SingapuRateUtils.SR_Utilities.SingapuRatePrefKeyCurDomainUrlIdx;
@@ -312,12 +280,13 @@ window.onload = function() {
 	}
 	//chrome.tabs.create( { url : voteUrl  }, function() {} ); 
    	window.close();
+   	//*/
 	
   };
 
   document.getElementById("b_why_matters").onclick = function() {
 
-	var voteUrl = "http://" + SingapuRateUtils.SR_Utilities.SingapuRateDomainName + "/viewtopic.php?f=12&t=1280";
+	//var voteUrl = "http://" + SingapuRateUtils.SR_Utilities.SingapuRateDomainName + "/viewtopic.php?f=12&t=1280";
 	//chrome.tabs.create( { url : voteUrl  }, function() {} ); 
     window.close();
   };
