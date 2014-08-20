2485d6b0b1a15c151c6adb62f1c4f21a47b448f0
sandodo <zouxiong@gmail.com>
2014-08-20 10:55:42 +0800

    now popup panel works for save
    continue working on deregister

--- data/popup.js
+++ data/popup.js
@@ -16,6 +16,7 @@ self.port.on("initMsg", function(msg) {
 	var sCurDomainUrlIdx = "";
 	var sSuperSafeMode = "";
 	var sVoteId = "";
+	var sAuthenticateCode = "";
 	
 	for(var i=0; i < resArray.length; i++) 
 	{
@@ -49,6 +50,10 @@ self.port.on("initMsg", function(msg) {
 		{
 			sVoteId = sTempKeyValuePair[1];
 		}
+		else if (sTempKeyValuePair[0] == "authencode")
+		{
+			sAuthenticateCode = sTempKeyValuePair[1];
+		}
 	}
 	//*/
 	if(sCurDomainUrlIdx == "")
@@ -58,6 +63,8 @@ self.port.on("initMsg", function(msg) {
 			
 	document.getElementById("c_username").value = sUserName;
 	document.getElementById("c_birthday").value = sUserBirthday;
+	document.getElementById("authencode").value = sAuthenticateCode;
+	document.getElementById("authenticate").value = sAuthenticate;
 	
   	if( sAuthenticate == "true"  )
   	{
@@ -117,45 +124,122 @@ self.port.on("initMsg", function(msg) {
 
 
 window.onload = function() {
-  document.getElementById("b_save").onclick = function() {
-    var username = document.getElementById("c_username").value.trim();
-	var password = document.getElementById("c_password").value.trim();
-	var birthday = document.getElementById("c_birthday").value.trim();
-	var supersafe = document.getElementById("c_supersafe").value.trim();
+  	document.getElementById("b_save").onclick = function() {
+	  	console.log("b_save" + ": " + "clicked");
+    	var username = document.getElementById("c_username").value.trim();
+		var password = document.getElementById("c_password").value.trim();
+		var birthday = document.getElementById("c_birthday").value.trim();
+		var supersafe = document.getElementById("c_supersafe").value.trim();
+		var sAuthenticateCode = document.getElementById("authencode").value.trim();
+		var sAuthenticate = document.getElementById("authenticate").value.trim();
 	
-	if( username.length < 4 || password.length < 4 )
-	{
-	  //it is a wrong username or password
-	  //alert(chrome.i18n.getMessage("SingapuRate_wrong_username_or_password", [4]));
-	  alert("SingapuRate_wrong_username_or_password");
-	  return;
-	}
-	else
-	{
-		var resArray = birthday.split("-");
-		if(resArray.length != 3)
+		//console.log("b_save" + ": " + "check " + username + " & " + password);
+		if( username.length < 4 || password.length < 4 )
 		{
-		  //it is a wrong birthday format
-	  	  //alert(chrome.i18n.getMessage("SingapuRate_wrong_birthday"));
-	  	  alert("SingapuRate_wrong_birthday");
-		  return;
+	  		//it is a wrong username or password
+	  		//alert(chrome.i18n.getMessage("SingapuRate_wrong_username_or_password", [4]));
+	  		alert("SingapuRate_wrong_username_or_password");
+	  		return;
 		}
 		else
 		{
-			//compose the string for profile save
-			var sPostMsg = "";
-			sPostMsg += "c_username" + "===" + username + "|||";
-			sPostMsg += "c_password" + "===" + password + "|||";
-			sPostMsg += "c_birthday" + "===" + birthday + "|||";
-			sPostMsg += "c_supersafe" + "===" + supersafe + "|||";
-			
-			self.port.emit("save", sPostMsg);
-
+			//console.log("b_save" + ": " + "check " + birthday);
+			var resArray = birthday.split("-");
+			if(resArray.length != 3)
+			{
+		  		//it is a wrong birthday format
+	  	  		//alert(chrome.i18n.getMessage("SingapuRate_wrong_birthday"));
+	  	  		alert("SingapuRate_wrong_birthday");
+		  		return;
+			}
+			else
+			{
+				if( sAuthenticate == "true"  )
+				{
+					//console.log("b_save" + ": " + "check " + sAuthenticate);
+		        	var passwordCombineStr = username.trim() + password.trim() + 'nh4da68h4jf6s4kj8g6d4df8b4d5';
+					var passwordEncrypted = CryptoJS.MD5(passwordCombineStr);
+					if( passwordEncrypted == sAuthenticateCode )
+					{
+						//authenticate the user with password, proceed to update profile
+					}
+					else
+					{
+						//failed to authenticate
+	  	  				alert("SingapuRate_update_fail");
+						return;
+					}
+				}
+			}
 		}
-	}
 		
-    return;
-  };
-      
+		alert("SingapuRate_update_success");
+			
+		//compose the string for profile save
+		var sPostMsg = "";
+		sPostMsg += "c_username" + "===" + username + "|||";
+		sPostMsg += "c_password" + "===" + password + "|||";
+		sPostMsg += "c_birthday" + "===" + birthday + "|||";
+		sPostMsg += "c_supersafe" + "===" + supersafe + "|||";
+			
+		self.port.emit("save", sPostMsg);
+		//*/		
+    	return;
+  	};
+
+  	document.getElementById("b_deregister").onclick = function() {
+	  	console.log("b_deregister" + ": " + "clicked");
+    	var username = document.getElementById("c_username").value.trim();
+		var password = document.getElementById("c_password").value.trim();
+		var birthday = document.getElementById("c_birthday").value.trim();
+		var sAuthenticateCode = document.getElementById("authencode").value.trim();
+		var sAuthenticate = document.getElementById("authenticate").value.trim();
+			
+		if( username.length < 4 || password.length < 4 )
+		{
+	  		//it is a wrong username or password
+	  		//alert(chrome.i18n.getMessage("SingapuRate_wrong_username_or_password", [4]));
+	  		alert("SingapuRate_wrong_username_or_password");
+	  
+	  		return;
+		}
+		else
+		{
+			if( sAuthenticate == "true"  )
+			{
+	        	var passwordCombineStr = username.trim() + password.trim() + 'nh4da68h4jf6s4kj8g6d4df8b4d5';
+				var passwordEncrypted = CryptoJS.MD5(passwordCombineStr);
+				if( passwordEncrypted == sAuthenticateCode )
+				{
+					//authenticate the user with password, proceed to deregister the profile
+				}
+				else
+				{
+					//failed to authenticate
+	  				//alert(chrome.i18n.getMessage("SingapuRate_logout_fail", [username]));
+	  				alert("SingapuRate_logout_fail");
+				
+					return;
+				}
+			}
+			else
+			{
+  				//alert(chrome.i18n.getMessage("SingapuRate_already_logout"));
+  				alert("SingapuRate_already_logout");
+				return;
+			}
+		}
+	
+		alert("SingapuRate_logout_success");
+		
+		//compose the string for profile save
+		var sPostMsg = "";
+		sPostMsg += "c_username" + "===" + username + "|||";
+		sPostMsg += "c_birthday" + "===" + birthday + "|||";
+			
+		self.port.emit("deregister", sPostMsg);
+									
+    	return;	  
+  };  	      
 };
 
